b9286068db86367a59fd6a38b689b37e0d495b3c
Subush <99478816+Subush@users.noreply.github.com>
2022-08-23 16:27:01 +0545

    Create passwordgen.py

---
+++ tools/passwordgen.py
@@ -0,0 +1,10 @@
+import random
+letters = "q w e r t y u i o p a s d f g h j k l z x c v b n m 1 2 3 4 5 6 7 8 9 0 @ ! # $ ^ & * ( ) _ < ? | Q W E R T Y U I O P A S D F G H J K L Z X C V B N M"
+z = letters.split(" ")
+for i in range(5):
+    random_shit=""
+    for i in range(random.randint(8,12)):
+     hm  = random.choice(z)
+     random_shit=random_shit+hm
+    with open("passwords.txt","a+")as f:
+        f.write(random_shit+"\n")
